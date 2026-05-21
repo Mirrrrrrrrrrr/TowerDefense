37c378218ec5d477e359c4d3d57e38799c0e4e19
Mirrrrrrrrrrr <101672763+Mirrrrrrrrrrr@users.noreply.github.com>
2026-05-21 17:25:44 +0700

    Update map.py

--- map.py
+++ map.py
@@ -13,24 +13,23 @@ COLOR_PATH  = (160, 130,  80)  # coklat pasir
 COLOR_SPAWN = ( 50, 150, 220)  # biru
 COLOR_BASE  = (220,  60,  60)  # merah
 COLOR_GRID  = ( 20,  60,  20)  # garis grid
-COLOR_HOVER = (255, 255, 100)  # highlight hover
 
 
 class Cell():
-    def __init__(self, col: int, row: int, tileType: int = TILE_EMPTY, 
+    def __init__(self, col: int, row: int, _tileType: int = TILE_EMPTY, 
                  isOccupied: bool = False, _cellSize: int = 48, 
                  _offset_x: int = 0, _offset_y: int = 0):
         self.col = col
         self.row = row
-        self.tileType = tileType
-        self.isOccupied = isOccupied
+        self._tileType = _tileType
+        self.__occupied = False
         self._cellSize = _cellSize
         self._offset_x = _offset_x
         self._offset_y = _offset_y
-        self.image = None
-
+        self.image: pg.Surface = None
+        
     def get_rect(self) -> pg.Rect:          
-        x = self.col * self._cellSize # x = self._offset_x + self.col * self._cellSize untuk HUD kiri
+        x = self.col * self._cellSize 
         y = self.row * self._cellSize
         return pg.Rect(x, y, self._cellSize, self._cellSize)
 
@@ -39,33 +38,38 @@ class Cell():
         return (r.centerx, r.centery) 
 
     def is_path(self) -> bool:
-        return self.tileType in (TILE_PATH, TILE_SPAWN, TILE_BASE)
+        return self._tileType in (TILE_PATH, TILE_SPAWN, TILE_BASE)
+
+    def occupy(self):
+        self.__occupied = True
+
+    def is_occupied(self):
+        return self.__occupied
 
     def can_place_turret(self) -> bool:
-        return self.tileType == TILE_EMPTY and not self.isOccupied
+        return self._tileType == TILE_EMPTY and not self.is_occupied()
 
     def draw(self, surface: pg.Surface,
             show_grid: bool = True,
             hovered: bool = False):
         rect = self.get_rect()
 
-        if self.tileType == TILE_EMPTY:
+        if self._tileType == TILE_EMPTY:
             if Assets.TILE_EMPTY:
                 surface.blit(Assets.TILE_EMPTY, rect)
             else:
                 pg.draw.rect(surface, COLOR_EMPTY, rect)
-
         elif self.is_path():
+            
             if self.image:
                 surface.blit(self.image, rect.topleft)
-             # Fallback warna jika gambar gagal dimuat
-            else: 
-                if self.tileType == TILE_SPAWN:
+            else:
+                # Fallback warna jika gambar gagal dimuat
+                if self._tileType == TILE_SPAWN:
                     color = COLOR_SPAWN 
-                elif self.tileType == TILE_BASE:
-                    color = COLOR_BASE
-                else: 
-                    color = COLOR_PATH
+                elif self._tileType == TILE_BASE: 
+                    color = COLOR_BASE 
+                else: color = COLOR_PATH
                 pg.draw.rect(surface, color, rect)
 
         # hover highlight 
@@ -79,12 +83,11 @@ class Cell():
             pg.draw.rect(surface, COLOR_GRID, rect, 1)
 
 
-
 class Grid:
     def __init__(
         self,
-        cols = 20,
-        rows = 10,
+        cols = 16,
+        rows = 16,
         cellSize = 48,
         offset_x = 0,
         offset_y = 0,
@@ -113,9 +116,6 @@ class Grid:
             else self._defaultWaypoints()
         ) # titik-titik tujuan
 
-        self.rockyTiles: list[tuple[int, int]] = [
-            (0, 0),
-        ]
         self.path: list[Cell] = []
         self.world_path: list[tuple[float, float]] = []
         
@@ -140,43 +140,42 @@ class Grid:
             (15, 1),
         ]
     
-    # untuk memetakan waypoints dan cells diantaranya pada path
+    # Untuk memetakan waypoints dan cells diantaranya pada path
     def _build_path(self):
         self.path = []
 
-        for i, (c1, r1) in enumerate(self.waypoints): # memetakan waypoints
-            is_last = (i == len(self.waypoints) - 1)
-            if i < len(self.waypoints) - 1:
-                c2, r2 = self.waypoints[i + 1]
-            else:
-                c2, r2 = c1, r1
-
-            segment = self._segment_cells(c1, r1, c2, r2, include_end = is_last) # memetakan cells antar waypoints
+        # Memetakan segmen pasangan 
+        for i in range(len(self.waypoints) - 1):
+            c1, r1 = self.waypoints[i]
+            c2, r2 = self.waypoints[i + 1]
+            is_last_segment = (i == len(self.waypoints) - 2)
+            # Menghubunkgan waypoints
+            segment = self._segment_cells(c1, r1, c2, r2, include_end = is_last_segment)
             for (c, r) in segment:
                 if (c, r):
                     cell = self.cells[r][c]
 
-                    if (c,r) == self.waypoints[0]:
-                        cell.tileType = TILE_SPAWN
+                    if (c, r) == self.waypoints[0]:
+                        cell._tileType = TILE_SPAWN
                     elif (c, r) == self.waypoints[-1]:
-                        cell.tileType = TILE_BASE
+                        cell._tileType = TILE_BASE
                     else:
-                        cell.tileType = TILE_PATH
+                        cell._tileType = TILE_PATH
                     self.path.append(cell)
                     
-        self._update_path_image()
+        self._update_path_images()
 
-    def _update_path_image(self):
+    def _update_path_images(self):
         # Nilai tetangga: Atas=1, Kanan=2, Bawah=4, Kiri=8
         BITMASK_MAP = {
-            1: Assets.TILE_STRAIGHT_V,    2: Assets.TILE_STRAIGHT_H,
-            4: Assets.TILE_STRAIGHT_V,    8: Assets.TILE_STRAIGHT_H,
-            5: Assets.TILE_STRAIGHT_V,    10: Assets.TILE_STRAIGHT_H,
-            3: Assets.TILE_CORNER_RU,     6: Assets.TILE_CORNER_RD,
-            9: Assets.TILE_CORNER_LU,     12: Assets.TILE_CORNER_LD,
-            13: Assets.TILE_T_NO_R,       11: Assets.TILE_T_NO_D,
-            7: Assets.TILE_T_NO_L,        14: Assets.TILE_T_NO_U,
-            15: Assets.TILE_CROSS,
+            1: Assets.TILE_STRAIGHT_V,  2: Assets.TILE_STRAIGHT_H,
+            4: Assets.TILE_STRAIGHT_V,  8: Assets.TILE_STRAIGHT_H,
+            5: Assets.TILE_STRAIGHT_V, 10: Assets.TILE_STRAIGHT_H,
+            12: Assets.TILE_CORNER_LD,  9: Assets.TILE_CORNER_LU,
+            6: Assets.TILE_CORNER_RD,   3: Assets.TILE_CORNER_RU,
+            13: Assets.TILE_T_NO_R,    11: Assets.TILE_T_NO_D,
+            7: Assets.TILE_T_NO_L,     14: Assets.TILE_T_NO_U,
+            15: Assets.TILE_CROSS
         }
 
         for row in range(self.rows):
@@ -184,11 +183,11 @@ class Grid:
                 cell = self.cells[row][col]
                 if cell.is_path():
                     mask = 0
-                    # Cek cell tetangga apakah cell tetangga merupakan jalan 
-                    if row > 0 and self.cells[row-1][col].is_path(): mask += 1
-                    if col > 0 and self.cells[row][col-1].is_path(): mask += 8
-                    if row < self.rows - 1 and self.cells[row+1][col].is_path(): mask += 4
-                    if col < self.cols - 1 and self.cells[row][col+1].is_path(): mask += 2
+                    # Cek cell tetangga apakah cell tetangga merupakan jalan
+                    if row > 0 and self.cells[row-1][col].is_path(): mask += 1            # Atas
+                    if col < self.cols - 1 and self.cells[row][col+1].is_path(): mask += 2 # Kanan
+                    if row < self.rows - 1 and self.cells[row+1][col].is_path(): mask += 4 # Bawah
+                    if col > 0 and self.cells[row][col-1].is_path(): mask += 8            # Kiri
 
                     cell.image = BITMASK_MAP.get(mask, None)
 
@@ -237,3 +236,5 @@ class Grid:
             for cell in row:
                 hovered = (cell is hovered_cell)
                 cell.draw(surface, show_grid = show_grid, hovered = hovered)
+
+        
