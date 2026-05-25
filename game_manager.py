ff10190c0443130fc0d5728fc1c153bf94d862cc
Mirrrrrrrrrrr <101672763+Mirrrrrrrrrrr@users.noreply.github.com>
2026-05-25 12:55:59 +0700

    Update game_manager.py

--- game_manager.py
+++ game_manager.py
@@ -11,16 +11,17 @@ class gameSpeedCtrl:
   SPEEDS: list[float] = [1.0, 2.0]
 
   def __init__(self):
-    self.paused : bool = false
+    self.paused : bool = False
     self._speedIdx: int = 0
 
-  def speed(self) -> float;
+  @property
+  def speed(self) -> float:
     return self.SPEEDS[self._speedIdx]
 
-  def togglePause(self):
+  def toggle_pause(self):
     self.paused = not self.paused
 
-  def cycleSpeed(self):
+  def cycle_speed(self):
     self._speedIdx = (self._speedIdx + 1) % len(self.SPEEDS)
 
   def getDt(self, rawDt: float) -> float:
@@ -28,17 +29,30 @@ class gameSpeedCtrl:
 
 
 
-
 # ========== game manager ==========
 class GameManager:
   lives: int = 100
   gold: int = 0
+  wave: int = 1
+  selected_cell = None
   gameOver: bool = False
   victory: bool = False
 
-  
-  
   turrets: list = []
   enemies: list = []
   projectile: list = []
+
+  @classmethod
+  def add_turret(cls, turret_class, cell) -> bool:
+    """Mencoba membeli dan menempatkan turret baru di cell."""
+    if cell.can_place_turret():
+      if cls.gold >= turret_class.cost:
+        cls.gold -= turret_class.cost
+        new_turret = turret_class(cell)
+        cls.turrets.append(new_turret)
+        cell.occupy()
+        return True
+      else:
+        print(f"Gold tidak cukup! Harga: {turret_class.cost}, Gold kamu: {cls.gold}")
+    return False
   
